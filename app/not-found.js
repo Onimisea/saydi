3d8e3c6dc050092c70b3a6d340a36b1b05a82cc4
Onimisea <onimisea@gmail.com>
2023-07-23 12:03:37 +0100

    Added custom not found page

--- app/not-found.js
+++ app/not-found.js
@@ -10,7 +10,7 @@ export default function NotFound() {
   return (
     <section className="body__container">
       <main className="bg-gradient-primary flex items-center justify-center h-screen">
-        <section className="bg-white p-10 rounded-md w-1/2 flex flex-col items-center justify-center gap-4 text-center">
+        <section className="bg-white p-10 rounded-md w-[90%] md:w-[70%] lg:w-1/2 flex flex-col items-center justify-center gap-6 text-center antialiased">
           <p className="text-lg">
             Hello there, the page you're looking for is not available at the
             moment. Please, check back later!
@@ -18,7 +18,7 @@ export default function NotFound() {
 
           <buttom
             onClick={() => router.back()}
-            className="rounded-md bg-primary px-4 py-2 text-white uppercase text-sm hover:bg-secondary duration-500"
+            className="rounded-md bg-primary px-4 py-2 text-white uppercase text-sm hover:bg-secondary duration-500 font-bold cursor-pointer"
           >
             Go Back
           </buttom>

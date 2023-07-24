77a27ece79ab25ce45c2feacd05a8e3f43927331
Ahmed (Onimisea) Tijani <onimisea@gmail.com>
2023-07-24 23:42:25 +0100

    Update Sidebar.js

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -94,9 +94,9 @@ const Sidebar = () => {
     {navOpen && (
       <motion.aside className="w-full h-full z-40 absolute top-0 bg-white py-10 " 
     
-initial={{ x: 300, opacity: 0 }}
+initial={{ x: "-100%", opacity: 0 }}
     animate={{ x: 0, opacity: 1 }}
-    exit={{ x: 300, opacity: 0 }}
+    exit={{ x: "100%", opacity: 0 }}
     transition={{
       type: "spring",
       stiffness: 260,

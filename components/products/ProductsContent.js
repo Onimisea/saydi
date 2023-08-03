716d6d32376beac1039c2a98196cdd112b257de1
Ahmed (Onimisea) Tijani <onimisea@gmail.com>
2023-08-03 10:04:54 +0100

    Update ProductsContent.js

--- components/products/ProductsContent.js
+++ components/products/ProductsContent.js
@@ -17,7 +17,29 @@ const ProductsContent = () => {
 
       <section className="content__container w-full z-20">
     <section className="flex flex-col items-center justify-center py-12 ">
-    { productsData.map((data) => (<p>{data.title}</p>)) }
+    { productsData.map((data, i) => (
+      <section key={i} className="">
+      <section className="w-full h-auto lg:w-[47%] flex flex-col gap-6">
+      <section className="w-full flex items-center justify-center gap-2">
+<h3 className="">{data.title}</h3>
+<HeaderDivider style="secondary" />
+</section>
+      
+      <section className="">{data.excerpt}</section>
+      </section>
+
+      <section className="w-full h-auto lg:w-[47%] rounded-tl-[32px] rounded-br-[32px]">
+      <Image
+    src={data.featured_image}
+    alt={data.title}
+    width={600}
+    height={600}
+    className="w-full h-auto object-fill z-10"
+  loading="lazy"
+/></section>
+      
+      </section>
+    )) }
     </section>
     </section>
     </section>
@@ -26,12 +48,5 @@ const ProductsContent = () => {
 export default ProductsContent
 
 
-// <Image
-//         src={herobbImg}
-//         alt="Hero Bubble"
-//         width={600}
-//         height={600}
-//         className="absolute top-[-10px] lg:right-[-20%] w-full lg:w-[67%] h-full lg:h-auto object-fill z-10"
-//         loading="lazy"
-//       />
+
       

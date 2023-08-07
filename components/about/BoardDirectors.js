9238c53a7ab6573bd10699fd60a6990070ee5d11
Onimisea <onimisea@gmail.com>
2023-08-07 11:15:29 +0100

    .

--- components/about/BoardDirectors.js
+++ components/about/BoardDirectors.js
@@ -3,23 +3,18 @@
 import { AnimatePresence, motion } from "framer-motion";
 import Image from "next/image";
 
-import herobbImg from "../../public/assets/home/hero_section_bb.png";
-import about_hero from "../../public/imgs/about/about_hero.png";
+import about_img1 from "../../public/imgs/about/about_img1.png";
+import about_img2 from "../../public/imgs/about/about_img2.png";
+import about_img3 from "../../public/imgs/about/about_img3.png";
+import about_img4 from "../../public/imgs/about/about_img4.png";
+import about_img5 from "../../public/imgs/about/about_img5.png";
+import about_img6 from "../../public/imgs/about/about_img6.png";
+
 import HeaderDivider from "../HeaderDivider";
 
 const BoardDirectors = () => {
   return (
     <section className="w-full h-auto flex flex-col relative ">
-      {/*       
-      <Image
-        src={herobbImg}
-        alt="Hero Bubble"
-        width={600}
-        height={600}
-        className="absolute top-[-5px] right-[-30%] w-full h-auto object-fill z-10"
-        loading="lazy"
-      /> */}
-
       <section className="content__container w-full z-20">
         <section className="w-full h-auto flex flex-col items-center justify-start py-12">
           <h1 className="text-secondary font-gillsans_heavy font-[900] uppercase text-[20px] md:text-[36px] lg:text-[42px]">
@@ -27,6 +22,31 @@ const BoardDirectors = () => {
           </h1>
           <section className="w-[50%] md:w-[25%] lg:w-[15%]">
             <HeaderDivider style="primary" />
+          </section>
+
+          <section className="w-full flex flex-wrap items-start justify-center gap-8 mt-8">
+            <section className="overflow-hidden relative w-[47%]">
+              <Image
+                src={about_img1}
+                alt="Dr. Amina Salihu"
+                width={600}
+                height={600}
+                className="w-full h-auto object-contain z-10"
+                loading="lazy"
+              />
+
+              <section className="w-full m-4 rounded-[16px] bg-white shadow-custom p-6">
+                <span
+                  className={`font-[700] text-[17px] ${
+                    donateType === 0
+                      ? "bg-gradient-to-r from-secondary to-primary text-transparent bg-clip-text"
+                      : "text-white"
+                  }`}
+                >
+                  Give Once
+                </span>
+              </section>
+            </section>
           </section>
         </section>
       </section>

46715c29c70ef15a063c7247be0576a4aed5a818
Ahmed (Onimisea) Tijani <onimisea@gmail.com>
2023-08-21 08:22:15 +0100

    Update CareersContent.js

--- components/opportunities/CareersContent.js
+++ components/opportunities/CareersContent.js
@@ -16,18 +16,18 @@ const CareersContent = () => {
     <section className="w-full h-auto flex flex-col relative ">
       <section className="content__container w-full z-20">
         <section className="flex flex-wrap items-start justify-center px-4 py-24 gap-8 relative">
-          <section className="w-full flex flex-wrap gap-6 items-start justify-center">
-            <section className="w-[95%] sm2:w-[47%] md2:w-[32%] lg:w-[20%] flex flex-col items-center justify-center gap-8 z-20 rounded-[10px] bg-white shadow-md overflow-hidden">
+          <section className="w-full flex flex-wrap gap-6 items-center justify-center">
+            <section className="w-[95%] sm2:w-[47%] md2:w-[32%] lg:w-[20%] flex flex-col items-center justify-center gap-6 z-20 rounded-[10px] bg-white shadow-md overflow-hidden">
               <Image
                 src={hiring}
                 alt="We are hiring"
                 width={600}
                 height={600}
-                className="w-full h-[200px] object-fill z-10 rounded-tl-[10px] rounded-tr-[10px]"
+                className="w-full h-[230px] object-fill z-10 rounded-tl-[10px] rounded-tr-[10px]"
                 loading="lazy"
               />
 
-              <section className="w-full flex items-start justify-start p-4 gap-4">
+              <section className="w-full flex items-start justify-start p-6 gap-6">
 <Image
                 src={onimisea}
                 alt="Admin"
@@ -37,8 +37,8 @@ const CareersContent = () => {
                 loading="lazy"
               />
 
-                <section className="flex flex-col gap-2 text-[#1e1e1e] text-[13px]">
-                  <h4 className="uppercase font-[600] text-[15px]">Sales Associate</h4>
+                <section className="flex flex-col gap-2 text-[#1e1e1e] text-[14px]">
+                  <h4 className="uppercase font-[600] text-[16px]">Sales Associate</h4>
                   <p>XYZ COMPANY</p>
                   <p>LAGOS, NIGERIA</p>
               </section>
@@ -46,6 +46,229 @@ const CareersContent = () => {
 
               
             </section>
+
+
+
+ex-col items-center justify-center gap-6 z-20 rounded-[10px] bg-white shadow-md overflow-hidden">
+              <Image
+                src={hiring}
+                alt="We are hiring"
+                width={600}
+                height={600}
+                className="w-full h-[230px] object-fill z-10 rounded-tl-[10px] rounded-tr-[10px]"
+                loading="lazy"
+              />
+
+              <section className="w-full flex items-start justify-start p-6 gap-6">
+<Image
+                src={onimisea}
+                alt="Admin"
+                width={600}
+                height={600}
+                className="w-[50px] h-[50px] object-cover z-10 rounded-full"
+                loading="lazy"
+              />
+
+                <section className="flex flex-col gap-2 text-[#1e1e1e] text-[14px]">
+                  <h4 className="uppercase font-[600] text-[16px]">Sales Associate</h4>
+                  <p>XYZ COMPANY</p>
+                  <p>LAGOS, NIGERIA</p>
+              </section>
+              </section>
+
+              
+            </section>
+
+
+
+ex-col items-center justify-center gap-6 z-20 rounded-[10px] bg-white shadow-md overflow-hidden">
+              <Image
+                src={hiring}
+                alt="We are hiring"
+                width={600}
+                height={600}
+                className="w-full h-[230px] object-fill z-10 rounded-tl-[10px] rounded-tr-[10px]"
+                loading="lazy"
+              />
+
+              <section className="w-full flex items-start justify-start p-6 gap-6">
+<Image
+                src={onimisea}
+                alt="Admin"
+                width={600}
+                height={600}
+                className="w-[50px] h-[50px] object-cover z-10 rounded-full"
+                loading="lazy"
+              />
+
+                <section className="flex flex-col gap-2 text-[#1e1e1e] text-[14px]">
+                  <h4 className="uppercase font-[600] text-[16px]">Sales Associate</h4>
+                  <p>XYZ COMPANY</p>
+                  <p>LAGOS, NIGERIA</p>
+              </section>
+              </section>
+
+              
+            </section>
+
+
+
+ex-col items-center justify-center gap-6 z-20 rounded-[10px] bg-white shadow-md overflow-hidden">
+              <Image
+                src={hiring}
+                alt="We are hiring"
+                width={600}
+                height={600}
+                className="w-full h-[230px] object-fill z-10 rounded-tl-[10px] rounded-tr-[10px]"
+                loading="lazy"
+              />
+
+              <section className="w-full flex items-start justify-start p-6 gap-6">
+<Image
+                src={onimisea}
+                alt="Admin"
+                width={600}
+                height={600}
+                className="w-[50px] h-[50px] object-cover z-10 rounded-full"
+                loading="lazy"
+              />
+
+                <section className="flex flex-col gap-2 text-[#1e1e1e] text-[14px]">
+                  <h4 className="uppercase font-[600] text-[16px]">Sales Associate</h4>
+                  <p>XYZ COMPANY</p>
+                  <p>LAGOS, NIGERIA</p>
+              </section>
+              </section>
+
+              
+            </section>
+
+
+ex-col items-center justify-center gap-6 z-20 rounded-[10px] bg-white shadow-md overflow-hidden">
+              <Image
+                src={hiring}
+                alt="We are hiring"
+                width={600}
+                height={600}
+                className="w-full h-[230px] object-fill z-10 rounded-tl-[10px] rounded-tr-[10px]"
+                loading="lazy"
+              />
+
+              <section className="w-full flex items-start justify-start p-6 gap-6">
+<Image
+                src={onimisea}
+                alt="Admin"
+                width={600}
+                height={600}
+                className="w-[50px] h-[50px] object-cover z-10 rounded-full"
+                loading="lazy"
+              />
+
+                <section className="flex flex-col gap-2 text-[#1e1e1e] text-[14px]">
+                  <h4 className="uppercase font-[600] text-[16px]">Sales Associate</h4>
+                  <p>XYZ COMPANY</p>
+                  <p>LAGOS, NIGERIA</p>
+              </section>
+              </section>
+
+              
+            </section>
+
+
+ex-col items-center justify-center gap-6 z-20 rounded-[10px] bg-white shadow-md overflow-hidden">
+              <Image
+                src={hiring}
+                alt="We are hiring"
+                width={600}
+                height={600}
+                className="w-full h-[230px] object-fill z-10 rounded-tl-[10px] rounded-tr-[10px]"
+                loading="lazy"
+              />
+
+              <section className="w-full flex items-start justify-start p-6 gap-6">
+<Image
+                src={onimisea}
+                alt="Admin"
+                width={600}
+                height={600}
+                className="w-[50px] h-[50px] object-cover z-10 rounded-full"
+                loading="lazy"
+              />
+
+                <section className="flex flex-col gap-2 text-[#1e1e1e] text-[14px]">
+                  <h4 className="uppercase font-[600] text-[16px]">Sales Associate</h4>
+                  <p>XYZ COMPANY</p>
+                  <p>LAGOS, NIGERIA</p>
+              </section>
+              </section>
+
+              
+            </section>
+
+
+ex-col items-center justify-center gap-6 z-20 rounded-[10px] bg-white shadow-md overflow-hidden">
+              <Image
+                src={hiring}
+                alt="We are hiring"
+                width={600}
+                height={600}
+                className="w-full h-[230px] object-fill z-10 rounded-tl-[10px] rounded-tr-[10px]"
+                loading="lazy"
+              />
+
+              <section className="w-full flex items-start justify-start p-6 gap-6">
+<Image
+                src={onimisea}
+                alt="Admin"
+                width={600}
+                height={600}
+                className="w-[50px] h-[50px] object-cover z-10 rounded-full"
+                loading="lazy"
+              />
+
+                <section className="flex flex-col gap-2 text-[#1e1e1e] text-[14px]">
+                  <h4 className="uppercase font-[600] text-[16px]">Sales Associate</h4>
+                  <p>XYZ COMPANY</p>
+                  <p>LAGOS, NIGERIA</p>
+              </section>
+              </section>
+
+              
+            </section>
+
+
+
+ex-col items-center justify-center gap-6 z-20 rounded-[10px] bg-white shadow-md overflow-hidden">
+              <Image
+                src={hiring}
+                alt="We are hiring"
+                width={600}
+                height={600}
+                className="w-full h-[230px] object-fill z-10 rounded-tl-[10px] rounded-tr-[10px]"
+                loading="lazy"
+              />
+
+              <section className="w-full flex items-start justify-start p-6 gap-6">
+<Image
+                src={onimisea}
+                alt="Admin"
+                width={600}
+                height={600}
+                className="w-[50px] h-[50px] object-cover z-10 rounded-full"
+                loading="lazy"
+              />
+
+                <section className="flex flex-col gap-2 text-[#1e1e1e] text-[14px]">
+                  <h4 className="uppercase font-[600] text-[16px]">Sales Associate</h4>
+                  <p>XYZ COMPANY</p>
+                  <p>LAGOS, NIGERIA</p>
+              </section>
+              </section>
+
+              
+            </section>
+
+
           </section>
 
           <section className="w-fit mx-auto mb-6">

8273a0f0785539689f4a657e2c4c62c0b3a7d3cf
Ahmed (Onimisea) Tijani <onimisea@gmail.com>
2023-09-16 21:04:04 +0100

    Update page.js

--- app/our-stories/blog-post/[slug]/page.js
+++ app/our-stories/blog-post/[slug]/page.js
@@ -121,7 +121,7 @@ export default function Post() {
                       </h2>
 
                       {relatedContent.length > 0 ? (
-                        <section className="w-full h-auto md:h-[300px] flex flex-col items-center justify-center md:flex-row">
+                        <section className="w-full h-auto md:h-[300px] flex flex-col items-center justify-center md:justify-between md:bg-green-500 md:flex-row">
                           
                       
                       {relatedContent.map((c, i) => (
